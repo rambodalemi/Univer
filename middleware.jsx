090fab4c3fcb5c8909ad8f759b450fcc1ccc7c97
Rambod <rambod.alemi87@gmail.com>
2025-01-23 20:24:34 +0100

    fixed middleware

--- middleware.jsx
+++ middleware.jsx
@@ -1,24 +1,26 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define public routes that do not require authentication
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/about",
+  "/contact",
+  "/catalogs",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isPublicRoute(req)) {
-    return; // Allow public routes
+    return;
   }
 
-  // Protect all other routes
   const { sessionClaims } = await auth.protect();
 
-  // Access role from session claims
   const userRole = sessionClaims?.["role"];
   const isAdminRoute = req.nextUrl.pathname.startsWith("/panel");
 
-  // Debugging: Log the claims to ensure the data is being fetched correctly
   console.log("JWT Claims:", sessionClaims);
 
-  // Restrict `/panel` to users with `admin` role
   if (isAdminRoute && userRole !== "admin") {
     return new Response("Forbidden: Admin access only.", { status: 403 });
   }
